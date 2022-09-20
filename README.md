49dc56002af4cd0c88c8162d224a2603a783c7eb
Berlinst <gabrielgalvao994@gmail.com>
2022-09-20 11:29:07 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Netflix - Página Inicial
+Projeto frontend referente ao bootcamp "JavaScript Game Developer" ministrado pela  [Digital Innovation One](https://www.dio.me/), com o instrutor [Felipe Aguiar](https://github.com/felipeAguiarCode).
+
+### Atividade Original: 
+https://github.com/felipeAguiarCode/netflix-clone
+
+### Exemplo
+![Exercício Dark Mode e Light Mode](\img\exemplo.PNG)
---
+++ js/owl/setup.js
@@ -0,0 +1,59 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function min1(){
+    window.open("https://www.netflix.com/br/title/81160697")
+}
+
+function min2(){
+    window.open("https://www.netflix.com/br/title/81309354")
+}
+
+function min3(){
+    window.open("https://www.netflix.com/br/title/80057281")
+}
+
+function min4(){
+    window.open("https://www.primevideo.com/dp/amzn1.dv.gti.6075169f-0c38-4799-a895-5eb30e49350f?autoplay=0&ref_=atv_cf_strg_wb")
+}
+
+function min5(){
+    window.open("https://www.netflix.com/br/title/81002370")
+}
+
+function min6(){
+    window.open("https://www.netflix.com/br/title/80002479")
+}
+
+function min7(){
+    window.open("https://www.disneyplus.com/pt-br/movies/star-wars-o-despertar-da-forca-episodio-vii/1LEKJPDPeMr7")
+}
+
+function min8(){
+    window.open("https://www.netflix.com/br/title/80186863")
+}
+
+function min9(){
+    window.open("https://www.netflix.com/br/title/81038963")
+}
+
+function min10(){
+    window.open("https://www.netflix.com/br/title/70299855?source=35")
+}
